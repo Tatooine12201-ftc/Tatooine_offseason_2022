4575bf40a57b7c38b7d254a5bca2562e94fc98c9
Sz77 <63186233+Sz77@users.noreply.github.com>
2022-08-26 16:07:30 +0300

    made teleop use mec class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic/Teleop.java
@@ -5,64 +5,22 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.firstinspires.ftc.teamcode.mecanum;
+import org.firstinspires.ftc.teamcode.Mecanum;
 
 
 @TeleOp( name = "Teleop")
 public class Teleop extends LinearOpMode {
-
-    mecanum robot=  new mecanum(hardwareMap);
-    private final double MAX_SPEED = 1;
-
-
+    Mecanum mecanum = new Mecanum(hardwareMap);
     @Override
     public void runOpMode() throws InterruptedException {
-
-        robot.init(hardwareMap);
-
-
-        // defining motors
-        DcMotorEx flm = null;
-        DcMotorEx blm = null;
-        DcMotorEx frm = null;
-        DcMotorEx brm = null;
-
-        flm.setDirection(DcMotorEx.Direction.REVERSE);
-        blm.setDirection(DcMotorEx.Direction.REVERSE);
-        frm.setDirection(DcMotorEx.Direction.REVERSE);
-        brm.setDirection(DcMotorEx.Direction.REVERSE);
-
-        mecanum mecanum = new mecanum(hardwareMap);
-
-        mecanum = new mecanum(null);
-        // creating an array for the motor speeds
-
         waitForStart();
-        double[] motorSpeeds = new double[2];
-        double max =1;
-
-
-
-        while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-
-            flm.setPower(frontLeftPower);
-            blm.setPower(backLeftPower);
-            frm.setPower(frontRightPower);
-            brm.setPower(backRightPower);
+        while (opModeIsActive() && !isStopRequested()) {
+            mecanum.drive(
+                            gamepad1.left_stick_x,
+                            gamepad1.left_stick_y,
+                            gamepad1.right_stick_x,
+                            true);
         }
-
     }
 
 
