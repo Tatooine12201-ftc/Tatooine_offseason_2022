5468c141c4064c7649e1e663753271175a9f8287
Sz77 <63186233+Sz77@users.noreply.github.com>
2022-08-26 17:09:55 +0300

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic/Teleop.java
@@ -17,7 +17,7 @@ public class Teleop extends LinearOpMode {
         while (opModeIsActive() && !isStopRequested()) {
             mecanum.drive(
                             gamepad1.left_stick_x,
-                            gamepad1.left_stick_y,
+                            -gamepad1.left_stick_y,
                             gamepad1.right_stick_x,
                             true);
         }
